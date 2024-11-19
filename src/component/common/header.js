ddc695eabc579fec647363a280653cfcdf3fdd07
DevNitinKumar <nitin.kumar@kayosys.org>
2024-11-19 23:09:43 +0530

    minor fixes

--- src/component/common/header.js
+++ src/component/common/header.js
@@ -3,41 +3,34 @@ import logo from '../../assets/finight.png';
 
 function Header() {
     return (
-        <div className='container-fluid headerHeight'>
-            <div className='row'>
-                <div className='col-lg-6'>
-                    <img src={logo} alt="Logo" className="logo ms-3" />
-                </div>
-                <div className='col-lg-4 offset-lg-2'>
-                <div className="d-flex justify-content-between align-items-center">
-                        {/* Step 1 */}
-                        <div className="text-center">
-                            <div className="circle completed">✓</div>
-                            <span className="step-label active-label">Select Your Queries</span>
-                        </div>
+        <div className='row align-items-center headerHeight'>
+            <div className='col-lg-6'>
+                <img src={logo} alt="Logo" className="logo ms-3" />
+            </div>
+            <div className='col-lg-4 offset-lg-1'>
+                <div className="d-flex justify-content-between align-items-center mr-2">
 
-                        {/* Horizontal Line */}
-                        <div className="line"></div>
+                    <div className="text-center flex-vertical">
+                        <div className="circle completed">✓</div>
+                        <span className="step-label active-label">Select Your Queries</span>
+                    </div>
 
-                        {/* Step 2 */}
-                        <div className="text-center">
-                            <div className="circle">02</div>
-                            <span className="step-label">Get Answers</span>
-                        </div>
+                    <div className="line"></div>
 
-                        {/* Horizontal Line */}
-                        <div className="line"></div>
+                    <div className="text-center flex-vertical">
+                        <div className="circle">02</div>
+                        <span className="step-label">Get Answers</span>
+                    </div>
 
-                        {/* Step 3 */}
-                        <div className="text-center">
-                            <div className="circle">03</div>
-                            <span className="step-label">Take Expert Assistance</span>
-                        </div>
+                    <div className="line"></div>
+
+                    <div className="text-center flex-vertical">
+                        <div className="circle">03</div>
+                        <span className="step-label">Take Expert Assistance</span>
                     </div>
                 </div>
             </div>
         </div>
-
     );
 }
 
